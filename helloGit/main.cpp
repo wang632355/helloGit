21e9397c3ee8d37f9617fa54c76fe4370b92117e
gang.wang <gang.wang@deptrum.com>
2022-04-29 16:16:56 +0800

    push

--- helloGit/main.cpp
+++ helloGit/main.cpp
@@ -4,6 +4,6 @@ int main(void)
 {
 	Student s(19, "da");
 	s.show();
-	std::cout << "abcdefghijk h333o1212st xiugai " << std::endl;
+	std::cout << "abcdef444ghijk h333o1212st xiugai " << std::endl;
 	return 0;
 }
